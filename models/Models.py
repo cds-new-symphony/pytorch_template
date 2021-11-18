adbba59cc11aafb81ebf09f21eeeb1fc6241556e
Hannah Theint Myat Wai <54528127+hannahmyatwai@users.noreply.github.com>
2021-11-18 12:35:59 +0800

    modified to CNN_LSTM and CNN_biLSTM

--- models/Models.py
+++ models/Models.py
@@ -71,7 +71,7 @@ class simpleLinear(nn.Module):
         return output
     
     
-class CNN_LSTM(nn.Module):
+class CNN_biLSTM(nn.Module):
     def __init__(self,
                  spec_layer,
                  norm_mode,
@@ -127,6 +127,63 @@ class CNN_LSTM(nn.Module):
         
         output = {"prediction": pred,
                   "spectrogram": spec}
-        return output        
+        return output
+class CNN_LSTM(nn.Module):
+    def __init__(self,
+                 spec_layer,
+                 norm_mode,
+                 input_dim,
+                 hidden_dim=768,
+                 output_dim=88):
+        super().__init__()
+        
+        self.spec_layer = spec_layer
+        self.norm_layer = Normalization(mode=norm_mode)
+        
+        self.cnn = nn.Sequential(
+            # layer 0
+            nn.Conv2d(1, hidden_dim // 16, (3, 3), padding=1),
+            nn.BatchNorm2d(hidden_dim // 16),
+            nn.ReLU(),
+            # layer 1
+            nn.Conv2d(hidden_dim // 16, hidden_dim // 16, (3, 3), padding=1),
+            nn.BatchNorm2d(hidden_dim // 16),
+            nn.ReLU(),
+            # layer 2
+            nn.MaxPool2d((1, 2)),
+            nn.Dropout(0.25),
+            nn.Conv2d(hidden_dim // 16, hidden_dim // 8, (3, 3), padding=1),
+            nn.BatchNorm2d(hidden_dim // 8),
+            nn.ReLU(),
+            # layer 3
+            nn.MaxPool2d((1, 2)),
+            nn.Dropout(0.25),
+        )
+        self.fc = nn.Sequential(
+            nn.Linear((hidden_dim // 8) * (input_dim // 4), hidden_dim),
+            nn.Dropout(0.5)
+        )
+        
+        self.lstm = nn.LSTM(hidden_dim, hidden_dim//2, batch_first=True, num_layers=1, bidirectional=False)
+        
+        self.classifier = nn.Linear(hidden_dim, output_dim)
+        
+    def forward(self, x):
+        spec = self.spec_layer(x) # (B, F, T)
+        spec = torch.log(spec+1e-8)
+        spec = spec.transpose(1,2) # (B, T, F)
+        spec = self.norm_layer(spec)
+        spec = spec.unsqueeze(1) # (B, 1, T, F)
+
+        x = self.cnn(spec) # (B, hidden_dim//8, T, F//4)
+        x = x.transpose(1,2).flatten(2)
+        x = self.fc(x) # (B, T, hidden_dim//8*F//4)
+        x, _ = self.lstm(x)
+        
+        pred = self.classifier(x)
+        
+        output = {"prediction": pred,
+                  "spectrogram": spec}
+        return output    
         
         
